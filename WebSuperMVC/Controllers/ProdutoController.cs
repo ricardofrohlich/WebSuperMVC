71090ef1061c075d8bed702eb530d21803833a31
ricardofrohlich <ricardosma@gmail.com>
2025-05-19 19:59:09 -0300

    Adicionar arquivos de projeto.

---
+++ WebSuperMVC/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSuperMVC.Models;
+
+namespace WebSuperMVC.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var produtos = GetProducts();
+            return View(produtos); //returnando para a view Index
+        }
+
+        private List<Produto> GetProducts()
+        {
+            return new List<Produto>
+            {
+                new Produto { Id = 1, Descricao = "Coca cola", Preco = 10.00m },
+                new Produto { Id = 2, Descricao = "Pepsi", Preco = 20.00m },
+                new Produto { Id = 3, Descricao = "Guaraná", Preco = 30.00m }
+            };
+        }
+    }
+}
---
+++ WebSuperMVC/Models/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebSuperMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
